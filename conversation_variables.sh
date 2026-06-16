1ff3c5b630df87d13572b4780ee7a70e651389f2
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-16 21:22:12 +0530

    second script on varibales

---
+++ conversation_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Aditya: How are you buddy"
+echo "Naveen: Im good buddy"
+echo "Aditya: Shall we meet today?"
+echo "Naveen: Sure buddy, at what time?"
+echo "Aditya: Will meet by 6pm today"
+echo "Naveen: Sure but will fix the venue"
+echo "Aditya: Sure, Iam waiting for the place"
