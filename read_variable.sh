5488a6a233d57aeac021c256b6bd3ba5bc43ad38
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-16 21:37:17 +0530

    read variable commit

--- read_variable.sh
+++ read_variable.sh
@@ -4,3 +4,8 @@
 echo "Please enter username : "
 read USER_NAME
 echo "You have enetered user name as $USER_NAME"
+
+echo "Please enter your password here:"
+read -s PASSWORD
+echo "You have enetered password as $PASSWORD"
+
