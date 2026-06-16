c8f99dfff2b28b82ee432fa89ac791a9634dbe77
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-16 21:30:04 +0530

    command line arguments

---
+++ command_line_args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+freind_1=$1
+frined_2=$2
+
+echo "$freind_1: How are you buddy"
+echo "$frined_2: Im good buddy"
+echo "$freind_1: Shall we meet today?"
+echo "$frined_2: Sure buddy, at what time?"
+echo "$freind_1: Will meet by 6pm today"
+echo "$frined_2: Sure but will fix the venue"
+echo "$freind_1: Sure, Iam waiting for the place"
