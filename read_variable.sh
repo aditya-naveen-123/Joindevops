d6f7388eb068f4620199d14373ddc1a68e4ec7a7
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-16 21:45:46 +0530

    read variable commit

--- read_variable.sh
+++ read_variable.sh
@@ -9,3 +9,10 @@ echo "Please enter your password here:"
 read -s PASSWORD
 echo "You have enetered password as $PASSWORD"
 
+
+#execution time
+TIMESTAMP1=$(date +%s)
+sleep 10
+TIMESTAMP2=$(date +%s)
+echo "execution time is $((TIMESTAMP2-TIMESTAMP1))"
+
