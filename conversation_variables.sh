f1264ffc054442d0d478ef2128d83c66f30463e7
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-16 21:25:17 +0530

    second script on varibales

--- conversation_variables.sh
+++ conversation_variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Aditya: How are you buddy"
-echo "Naveen: Im good buddy"
-echo "Aditya: Shall we meet today?"
-echo "Naveen: Sure buddy, at what time?"
-echo "Aditya: Will meet by 6pm today"
-echo "Naveen: Sure but will fix the venue"
-echo "Aditya: Sure, Iam waiting for the place"
+freind_1 = "Aditya"
+frined_2 = "Naveen"
+
+echo "$freind_1: How are you buddy"
+echo "$frined_2: Im good buddy"
+echo "$freind_1: Shall we meet today?"
+echo "$frined_2: Sure buddy, at what time?"
+echo "$freind_1: Will meet by 6pm today"
+echo "$frined_2: Sure but will fix the venue"
+echo "$freind_1: Sure, Iam waiting for the place"
