ce232b85746045eb52b8a3aea1dff480faa53a70
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-17 20:55:01 +0530

    Conditional initial commit

--- conditionals.sh
+++ conditionals.sh
@@ -18,10 +18,15 @@ USERID=$((id -u))
 if [ $USERID -eq 0 ]; then
     echo "User is root user"
     dnf list installed $2
-elif [ $? -ne 0 ]; then
-    echo "Instalaling $2"
-    dnf install $2
 else
     echo "User is not root user, switch to root user and then execute this script again"
     exit 1
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Package already installed"
+    #dnf install $2
+elif [ $? -gt 0]; then
+    echo "Installing the required application : $2"
+    dnf install $2 -y
 fi
