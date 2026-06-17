73d80acfc5f8c62fb0a7073bb3acb83673542cf1
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-17 20:22:10 +0530

    Conditional initial commit

---
+++ conditionals.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20]; then
+    echo "Given number is greater than 20"
+elif [ $NUMBER -eq 20]; then
+    ehco "Given number is equal to 20"
+else
+    echo "Given number is less than 20"
+
+fi
