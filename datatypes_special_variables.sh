7bbb684aed4ed71db60a2512d2d88d07eab6e1be
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-17 17:58:28 +0530

    special vars

--- datatypes_special_variables.sh
+++ datatypes_special_variables.sh
@@ -7,3 +7,5 @@ echo "Which user is running: $USER"
 echo "Directory at which script is running : $PWD"
 echo "User's home directory : $HOME"
 echo "process ID of the script: $$"
+sleep 5 &
+echo "Background process ID : $!"
