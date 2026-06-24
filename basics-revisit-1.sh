66da48941bfe8fe855e5942ef01d9e84f2d1ec37
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-25 05:20:55 +0530

    revisit

--- basics-revisit-1.sh
+++ basics-revisit-1.sh
@@ -5,3 +5,11 @@ NAME2="NAVEEN"
 
 echo "User 1 name is $NAME"
 echo "User 2 name is $NAME2"
+
+NAME=$1
+NAME2=$2
+
+echo "Command line varibales ====================="
+
+echo "User 1 is $NAME"
+echo "User 2 is $NAME2"
