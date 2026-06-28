bf6f7f691c6b383ab1e84bdeed710f45fffbedda
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-28 11:40:22 +0530

    Special_varibales_revision

--- special_variables_revision.sh
+++ special_variables_revision.sh
@@ -4,3 +4,13 @@ echo "All varaibles passed : $@"
 echo "Number of variables passed : $#"
 echo "Script name : $0"
 echo "User running the script: $USER"
+echo "Use current wotrking directory : $PWD"
+echo "USer home directory: $HOME"
+echo "Process intance ID of the ecript run : $$"
+sleep 5 &
+echo "PID of the background command : $!"
+echo "Line nummber of the script : $LINENO"
+echo "Execution time of the script : $SECONDS"
+echo " Generate random number : $RANDOM"
+echo "Exit code of the script : $?"
+
