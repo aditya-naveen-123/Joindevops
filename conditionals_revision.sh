0d19572fe35e69f5d824f336d316064366ad08af
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-28 12:07:20 +0530

    conditionals revision

--- conditionals_revision.sh
+++ conditionals_revision.sh
@@ -2,4 +2,8 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Number is gretaer than 20"
+elif [ $NUMBER -lt 20]; then
+    echo "Number is lessthan 20"
+else
+    echo "Number is equal to 20"
 fi
