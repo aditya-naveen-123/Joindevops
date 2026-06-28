5c8678c1e3787d3acc80e51d0f93eccfe656b1ef
aditya-naveen-123 <adityanaveen111@gmail.com>
2026-06-28 15:28:25 +0530

    Intall package

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user"
@@ -12,10 +13,10 @@ fi
 # second argument -> exir code
 VALIDATE() {
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is failed" | tee -a $LOGS_FILE
+        echo $TIMESTAMP "[ERROR] Installing $1 is failed" | tee -a $LOGS_FILE
         exit
     else
-        echo "Installing $1 is SUCESS" | tee -a $LOGS_FILE
+        echo $TIMESTAMP "[SUCCESS] Installing $1 is SUCESS" | tee -a $LOGS_FILE
     fi
 }
 
@@ -23,9 +24,9 @@ for package in $@
 do
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -eq 0 ]; then
-        echo "$package already exists... SKIPPING" | tee -a $LOGS_FILE
+        echo $TIMESTAMP "[INFO} $package already exists... SKIPPING" | tee -a $LOGS_FILE
     else
-        echo "Installing Package $package"
+        echo $TIMESTAMP "[INFO} Installing Package $package"
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE $package $?
     fi
